0ad3537dd08234eb233b4f472f71c465dbd4012a
Feplyss <fellipe.bomfim10@gmail.com>
2023-05-07 00:39:18 -0300

    feat: api consumption

--- src/app/components/Dashboard/usuario-dashboard/arquivos-privados/arquivos-privados.component.ts
+++ src/app/components/Dashboard/usuario-dashboard/arquivos-privados/arquivos-privados.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Recurso } from 'src/app/models/Recurso';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { RecursoService } from 'src/app/services/recurso.service';
 
 @Component({
   selector: 'app-arquivos-privados',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArquivosPrivadosComponent implements OnInit {
 
-  constructor() { }
+  recursos : Recurso[];
+  idUsuarioLogado : string;
+
+  constructor(
+    private recursoService : RecursoService,
+    private authGuardService: AuthGuardService
+  ) { }
 
   ngOnInit(): void {
+    this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
+
+    this.recursoService.ObterRecursoPeloUsuarioId(this.idUsuarioLogado).subscribe(resultado=>{
+      this.recursos = resultado;        
+  }) 
   }
 
 }
--- src/app/components/Dashboard/usuario-dashboard/dados-usuario/dados-usuario.component.ts
+++ src/app/components/Dashboard/usuario-dashboard/dados-usuario/dados-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/Usuario';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-dados-usuario',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadosUsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuario : Usuario
+  idUsuarioLogado : string;
+
+  constructor(
+    private usuarioService : UsuariosService,
+    private authGuardService: AuthGuardService
+    ) { }
 
   ngOnInit(): void {
+    this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
+
+    this.usuarioService.ObterUsuarioPorId(this.idUsuarioLogado).subscribe(resultado=>{
+      this.usuario = resultado;
+    })
+  }
+
+  getImage(baseImage:string) : any{
+
+    let objectURL = 'data:image/png;base64,' + atob(baseImage);
+    return objectURL;
   }
 
 }
+
